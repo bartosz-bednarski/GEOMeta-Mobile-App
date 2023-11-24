d61dabe3a80ca3440deb30cdf5475a614616e300
Bartosz Bednarski <bambocha1997@gmail.com>
2023-11-24 12:48:52 +0100

    Created continent components and continents screen with nav.

---
+++ screens/Continent/ContinentScreen.js
@@ -0,0 +1,45 @@
+import { useLayoutEffect, useState } from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+const ContinentScreen = ({ navigation, route }) => {
+  const continentId = route.params.continentId;
+  const [continentData, setContinentData] = useState("");
+  const fetchContinentData = async () => {
+    const response = await fetch(
+      `https://geo-meta-rest-api.vercel.app/api/continents/${continentId}`,
+      { mode: "cors" }
+    );
+    const data = await response.json();
+    setContinentData(data.data);
+  };
+  useLayoutEffect(() => {
+    fetchContinentData();
+  }, []);
+  //DEMO
+  console.log(continentData);
+  if (continentData !== "") {
+    return (
+      <View style={styles.continentContainer}>
+        <FlatList
+          data={continentData}
+          keyExtractor={(item) => item.country}
+          renderItem={(item) => {
+            return <Text style={{ color: "black" }}>{item.item.country}</Text>;
+          }}
+        />
+        <Text>Continent screen {continentId}</Text>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  continentContainer: {
+    backgroundColor: "#7131B7",
+    paddingBottom: 40,
+    paddingTop: 30,
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+});
+export default ContinentScreen;
