65b270d27f3c1de8b6f7ccd44cc22ea4d9184b01
Bartosz Bednarski <bambocha1997@gmail.com>
2023-11-27 11:20:58 +0100

    Updated continent screen. Created coutry screen.

---
+++ components/Continent/CountryIconBox.js
@@ -0,0 +1,25 @@
+import { Pressable, Image, Text, StyleSheet } from "react-native";
+const CountryIconBox = ({ onPress, image, country }) => {
+  return (
+    <Pressable style={styles.countryBox} onPress={onPress}>
+      <Image style={styles.image} source={image} />
+      <Text style={styles.text}>{country}</Text>
+    </Pressable>
+  );
+};
+const styles = StyleSheet.create({
+  countryBox: {
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: 75,
+    height: 110,
+    marginHorizontal: 5,
+  },
+
+  text: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 12,
+  },
+});
+export default CountryIconBox;
--- navigators/ContinentsNavigator.js
+++ navigators/ContinentsNavigator.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ContinentsScreen from "../screens/Continents/ContinentsScreen";
 import ContinentScreen from "../screens/Continent/ContinentScreen";
 import HeaderIcon from "../ui/HeaderIcon";
+import CountryScreen from "../screens/Country/CountryScreen";
 const ContinentsNavigator = () => {
   const Stack = createNativeStackNavigator();
 
@@ -30,7 +31,24 @@ const ContinentsNavigator = () => {
           };
         }}
       />
-      {/* <Stack.Screen componet={ContinentScreen} /> */}
+      <Stack.Screen
+        name="Country"
+        component={CountryScreen}
+        options={({ route }) => {
+          const header = route.params.header;
+          return {
+            headerShown: true,
+            headerTitle: header,
+            headerRight: () => {
+              return <HeaderIcon navType="stack" />;
+            },
+            headerStyle: {
+              backgroundColor: "#7B41BB",
+            },
+            headerTintColor: "white",
+          };
+        }}
+      />
     </Stack.Navigator>
   );
 };
