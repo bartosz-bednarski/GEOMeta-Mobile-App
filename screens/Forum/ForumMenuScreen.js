c67bb30bb12e2dc0a86a730e541c8f2d28a18fc9
Bartosz Bednarski <bambocha1997@gmail.com>
2023-12-04 16:08:38 +0100

    Created topic component and handled topic data display after fetch

--- screens/Forum/ForumMenuScreen.js
+++ screens/Forum/ForumMenuScreen.js
@@ -1,12 +1,65 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
 import AddTopicMenu from "../../components/Forum/AddTopicMenu";
+import TopicMenu from "../../components/Forum/TopicMenu";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import LoaderOverlay from "../../ui/LoaderOverlay";
 
 const ForumMenuScreen = ({ route, navigation }) => {
+  const accessToken = useSelector((state) => state.authorization.accessToken);
+  const [isFetching, setIsFetching] = useState(false);
+  const [fetchedTopics, setFetchedTopics] = useState("");
+  const getTopics = async () => {
+    setIsFetching(true);
+    const response = await fetch(
+      "https://geo-meta-rest-api.vercel.app/api/forum/getTopics",
+      // "http://localhost:9001/api/forum/getTopics",
+      {
+        method: "GET",
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    const data = await response.json();
+    setFetchedTopics(data.data);
+    setIsFetching(false);
+    // console.log(data);
+  };
+  useEffect(() => {
+    getTopics();
+  }, []);
+  if (isFetching) {
+    return <LoaderOverlay />;
+  }
   return (
-    <View style={styles.forumContainer}>
-      <AddTopicMenu navigation={navigation} />
-      <Text>Forum kurde</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.forumContainer}>
+        <AddTopicMenu navigation={navigation} />
+
+        {fetchedTopics != "" &&
+          fetchedTopics.map((topic) => {
+            return (
+              <TopicMenu
+                id={topic._id}
+                key={topic._id}
+                comments={topic.comments
+                  .slice(topic.comments.length - 3)
+                  .reverse()}
+                commentsSum={topic.comments.length}
+                time={
+                  topic.comments[topic.comments.length - 1].date +
+                  " " +
+                  topic.comments[topic.comments.length - 1].time
+                }
+              />
+            );
+          })}
+      </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
@@ -15,7 +68,6 @@ const styles = StyleSheet.create({
     paddingBottom: 40,
     paddingHorizontal: 10,
     paddingTop: 30,
-    paddingBottom: 50,
   },
 });
 export default ForumMenuScreen;
